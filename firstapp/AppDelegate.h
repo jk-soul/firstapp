c88a0879b8378eff0cd2b0c3cc753c93ace2cb38
jzh225576@alibaba-inc.com <jzh225576@alibaba-inc.com>
2019-12-05 15:43:26 +0800

    Initial Commit

---
+++ firstapp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  firstapp
+//
+//  Created by ipad on 2019/12/5.
+//  Copyright © 2019 ipad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ firstapp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  firstapp
+//
+//  Created by ipad on 2019/12/5.
+//  Copyright © 2019 ipad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
